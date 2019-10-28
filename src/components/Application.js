3cd9524aeb5fd53f38045e0a2e429475947aa142
jomicm <singularity.jmcm@gmail.com>
2019-10-28 16:17:01 -0400

    Impement axios library to retrieve days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from './DayList';
 import "components/Application.scss";
 import Appointment from './Appointment';
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from 'axios';
 
 const appointments = [
   {
@@ -69,7 +52,17 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
+  const [day, setDay] = useState('Tuesday');
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/days');
+      console.log('result', result.data);
+      setDays(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
