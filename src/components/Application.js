045547cd88a628306afeb5f40eb25a0fd22a4600
jomicm <singularity.jmcm@gmail.com>
2019-10-28 20:16:58 -0400

    Start with custom hooks

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,72 +1,78 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import DayList from './DayList';
 import "components/Application.scss";
 import Appointment from './Appointment';
-import axios from 'axios';
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: '2pm',
-    interview: {
-      student: "Joe Doe",
-      interviewer: {
-        id: 3,
-        name: "Norman Robinson",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: '3pm',
-  },
-  {
-    id: 5,
-    time: '5pm',
-    interview: {
-      student: "Jane Deffao",
-      interviewer: {
-        id: 2,
-        name: "Lin Remstein",
-        avatar: "https://i.imgur.com/twYrpay.jpg",
-      }
-    }
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: '2pm',
+//     interview: {
+//       student: "Joe Doe",
+//       interviewer: {
+//         id: 3,
+//         name: "Norman Robinson",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   {
+//     id: 4,
+//     time: '3pm',
+//   },
+//   {
+//     id: 5,
+//     time: '5pm',
+//     interview: {
+//       student: "Jane Deffao",
+//       interviewer: {
+//         id: 2,
+//         name: "Lin Remstein",
+//         avatar: "https://i.imgur.com/twYrpay.jpg",
+//       }
+//     }
+//   }
+// ];
 
 export default function Application(props) {
-  const [day, setDay] = useState('Tuesday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.get('/api/days');
-      console.log('result', result.data);
-      setDays(result.data);
-    };
-    fetchData();
-  }, []);
 
+    const daysPromise = axios.get('/api/days');
+    const appointmentsPromise = axios.get('/api/appointments');
+    const interviewersPromise = axios.get('/api/interviewers');
+    Promise.all([daysPromise, appointmentsPromise, interviewersPromise])
+      .then(all => {
+        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
+      });
+  }, []);
+  const appointments = getAppointmentsForDay(state, state.day);
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Environment Setup" activity. */}
         <img
           className="sidebar--centered"
           src="images/logo.png"
@@ -74,8 +80,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          {/* <DayList days={days} day={day} setDay={day => setDay(day)}/> */}
-          <DayList days={days} day={day} setDay={setDay}/>
+          <DayList days={state.days} day={state.day} setDay={(newDay) => setState(prev => ({ ...prev, day: newDay})) }/>
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -84,8 +89,16 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {appointments.map((a, ix) => <Appointment key={'appointement_' + ix}  {...a} />)}
+        {appointments.map((a) => {
+          const interview = getInterview(state, a.interview);
+          return (
+            <Appointment 
+              key={a.id}  
+              id={a.id} 
+              time={a.time} 
+              interview={interview} />)}
+          )
+        } 
         <Appointment key="last" time="6pm" />
       </section>
     </main>
