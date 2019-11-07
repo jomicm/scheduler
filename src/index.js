f51d0e9ce99957d5cf7f90f433ed81d1d409431a
jomicm <singularity.jmcm@gmail.com>
2019-11-06 23:37:31 -0500

    Update index.js to integrate custom axios path

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 
 import "index.scss";
 import Application from "components/Application";
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 // Main rendered Application
 ReactDOM.render(<Application />, document.getElementById("root"));
